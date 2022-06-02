7ed44145fe5f89852680c0960ed3e0c541cb16d8
Simon kamau <simon.kamau@student.moringaschool.com>
2022-06-02 19:44:36 +0300

    commit tree 66f6a0d9903597fc7608b55a0f793e731851d7f7

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+const employee = {
+    name: "Hardy",
+    streetAddress: "Kay Street"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee };
+
+    newEmployee[key] = value;
+
+    return newEmployee;
+}
